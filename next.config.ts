4c9e491b8ba1d561e77ca2b055870eba1a80630c
Lucas F. Albuquerque <lucasdefrancaalbuquerque@gmail.com>
2025-03-11 14:07:19 +0000

    ?

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,21 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_WEBHOOK_N8N_RESPONSE: process.env.NEXT_PUBLIC_WEBHOOK_N8N_RESPONSE,
+  },
+  webpack(config) {
+    console.log("NEXT_PUBLIC_API_BASE_URL:", process.env.NEXT_PUBLIC_API_BASE_URL);
+    console.log("NEXT_PUBLIC_APP_URL:", process.env.NEXT_PUBLIC_APP_URL);
+    console.log("NEXT_PUBLIC_SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+    console.log("NEXT_PUBLIC_SUPABASE_ANON_KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+    console.log("NEXT_PUBLIC_WEBHOOK_N8N_RESPONSE:", process.env.NEXT_PUBLIC_WEBHOOK_N8N_RESPONSE);
+    return config;
+  }
 };
 
 export default nextConfig;
